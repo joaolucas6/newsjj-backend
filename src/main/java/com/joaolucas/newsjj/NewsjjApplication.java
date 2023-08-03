2d1b06d048cfa851c116cf0762d0123c90121c21
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-03 19:44:55 -0300

    Initial commit

---
+++ src/main/java/com/joaolucas/newsjj/NewsjjApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.newsjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsjjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsjjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/newsjj/NewsjjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.newsjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsjjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
