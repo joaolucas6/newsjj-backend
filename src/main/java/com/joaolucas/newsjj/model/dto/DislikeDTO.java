28d0d55bb337a8467ab0c699566d465904314844
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-04 16:23:11 -0300

    Creating DislikeDTO

---
+++ src/main/java/com/joaolucas/newsjj/model/dto/DislikeDTO.java
@@ -0,0 +1,33 @@
+package com.joaolucas.newsjj.model.dto;
+
+import com.joaolucas.newsjj.model.entities.dislikes.CommentDislike;
+import com.joaolucas.newsjj.model.entities.dislikes.NewsDislike;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class DislikeDTO {
+
+    private Long id;
+    private Long authorId;
+    private Long contentId;
+    private LocalDateTime instant;
+    private String dislikeType;
+
+    public DislikeDTO(CommentDislike commentDislike){
+        setId(commentDislike.getId());
+        setAuthorId(commentDislike.getAuthor().getId());
+        setContentId(commentDislike.getComment().getId());
+        setInstant(commentDislike.getInstant());
+        setDislikeType("Comment Dislike");
+    }
+
+    public DislikeDTO(NewsDislike newsDislike){
+        setId(newsDislike.getId());
+        setAuthorId(newsDislike.getAuthor().getId());
+        setContentId(newsDislike.getNews().getId());
+        setInstant(newsDislike.getInstant());
+        setDislikeType("News Dislike");
+    }
+}
