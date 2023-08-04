bdf58f4b04d342c7db5f2fac79ed67b4862b83c8
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-04 15:45:44 -0300

    Creating CommentDislike entity

--- src/main/java/com/joaolucas/newsjj/model/entities/dislikes/CommentDislike.java
+++ src/main/java/com/joaolucas/newsjj/model/entities/dislikes/CommentDislike.java
@@ -1,4 +1,34 @@
 package com.joaolucas.newsjj.model.entities.dislikes;
 
+import com.joaolucas.newsjj.model.entities.Comment;
+import com.joaolucas.newsjj.model.entities.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_comment_dislike")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@EqualsAndHashCode(of = "id")
 public class CommentDislike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Column(name = "instant")
+    private LocalDateTime instant;
+    
 }
