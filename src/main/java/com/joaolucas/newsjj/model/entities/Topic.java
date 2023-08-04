96a862e4cbc8bcf4d9134ae283f4fbef212200bd
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-04 15:28:33 -0300

    Creating Topic entity

--- src/main/java/com/joaolucas/newsjj/model/entities/Topic.java
+++ src/main/java/com/joaolucas/newsjj/model/entities/Topic.java
@@ -1,4 +1,35 @@
 package com.joaolucas.newsjj.model.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_topic")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode(of = "id")
 public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "news_topics",
+            joinColumns = @JoinColumn(name = "topic_id"),
+            inverseJoinColumns = @JoinColumn(name = "news_id")
+    )
+    private List<News> news;
+
 }
