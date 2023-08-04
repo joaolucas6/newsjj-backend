3000d722398c650c39a7278cb1d731b7b08b6c43
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-04 15:46:42 -0300

    Creating NewsDislike entity

--- src/main/java/com/joaolucas/newsjj/model/entities/dislikes/CommentDislike.java
+++ src/main/java/com/joaolucas/newsjj/model/entities/dislikes/CommentDislike.java
@@ -30,5 +30,5 @@ public class CommentDislike {
 
     @Column(name = "instant")
     private LocalDateTime instant;
-    
+
 }
--- src/main/java/com/joaolucas/newsjj/model/entities/dislikes/NewsDislike.java
+++ src/main/java/com/joaolucas/newsjj/model/entities/dislikes/NewsDislike.java
@@ -1,4 +1,34 @@
 package com.joaolucas.newsjj.model.entities.dislikes;
 
+import com.joaolucas.newsjj.model.entities.News;
+import com.joaolucas.newsjj.model.entities.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_news_dislike")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@EqualsAndHashCode(of = "id")
 public class NewsDislike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "news_id")
+    private News news;
+
+    @Column(name = "instant")
+    private LocalDateTime instant;
+
 }
