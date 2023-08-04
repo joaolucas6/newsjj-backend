b378c48e49a177594fecbba99a7597dc66c1be58
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-04 15:43:23 -0300

    Creating CommentLike entity

--- src/main/java/com/joaolucas/newsjj/model/entities/likes/CommentLike.java
+++ src/main/java/com/joaolucas/newsjj/model/entities/likes/CommentLike.java
@@ -1,4 +1,35 @@
 package com.joaolucas.newsjj.model.entities.likes;
 
+import com.joaolucas.newsjj.model.entities.Comment;
+import com.joaolucas.newsjj.model.entities.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_comment_like")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@EqualsAndHashCode(of = "id")
 public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Column(name = "instant")
+    private LocalDateTime instant;
+
+
 }
