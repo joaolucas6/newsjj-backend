02d52b355b23ea18ca72dd8df74105b3f3c319c5
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-04 16:11:15 -0300

    Creating NewsDTO

---
+++ src/main/java/com/joaolucas/newsjj/model/dto/NewsDTO.java
@@ -0,0 +1,38 @@
+package com.joaolucas.newsjj.model.dto;
+
+import com.joaolucas.newsjj.model.entities.News;
+import com.joaolucas.newsjj.model.entities.Topic;
+import com.joaolucas.newsjj.model.entities.dislikes.NewsDislike;
+import com.joaolucas.newsjj.model.entities.likes.NewsLike;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class NewsDTO {
+
+    private Long id;
+    private String title;
+    private String text;
+    private LocalDateTime instant;
+    private Long authorId;
+    private List<String> imagesUrl;
+    private List<Long> topicsId;
+    private List<Long> likesId;
+    private List<Long> dislikesId;
+
+    public NewsDTO(News news){
+
+        setId(news.getId());
+        setTitle(news.getTitle());
+        setText(news.getText());
+        setInstant(news.getInstant());
+        setAuthorId(news.getAuthor().getId());
+        setImagesUrl(news.getImagesUrl());
+        setTopicsId(news.getTopics().stream().map(Topic::getId).toList());
+        setLikesId(news.getLikes().stream().map(NewsLike::getId).toList());
+        setDislikesId(news.getDislikes().stream().map(NewsDislike::getId).toList());
+
+    }
+}
