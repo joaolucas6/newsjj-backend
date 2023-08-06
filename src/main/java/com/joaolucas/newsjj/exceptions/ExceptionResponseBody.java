bc232368d5cf63b5ca574b02be21213013623de3
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-05 21:54:12 -0300

    Creating ExceptionResponseBody

---
+++ src/main/java/com/joaolucas/newsjj/exceptions/ExceptionResponseBody.java
@@ -0,0 +1,8 @@
+package com.joaolucas.newsjj.exceptions;
+
+import java.time.LocalDateTime;
+
+public record ExceptionResponseBody (
+        String error, Integer errorCode, String message, LocalDateTime timestamp
+){
+}
