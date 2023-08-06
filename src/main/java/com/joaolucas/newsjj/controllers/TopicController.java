68b5e110a8fc0cedfd3ec700894403b0a3a5f5cf
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-06 11:49:38 -0300

    Creating TopicController

---
+++ src/main/java/com/joaolucas/newsjj/controllers/TopicController.java
@@ -0,0 +1,45 @@
+package com.joaolucas.newsjj.controllers;
+
+import com.joaolucas.newsjj.model.dto.TopicDTO;
+import com.joaolucas.newsjj.services.TopicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/topics")
+@RequiredArgsConstructor
+public class TopicController {
+
+    private final TopicService topicService;
+
+    @GetMapping
+    public ResponseEntity<List<TopicDTO>> findAll(){
+        return ResponseEntity.ok(topicService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TopicDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(topicService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TopicDTO> create(@RequestBody TopicDTO topicDTO){
+        return ResponseEntity.ok(topicService.create(topicDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TopicDTO> update(@PathVariable Long id, @RequestBody TopicDTO updateRequest){
+        return ResponseEntity.ok(topicService.update(id, updateRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        topicService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
