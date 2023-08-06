5cf4863299d425f0dd7a369ceec1d425407fa096
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-05 22:03:58 -0300

    Handling ResourceNotFoundException

--- src/main/java/com/joaolucas/newsjj/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/joaolucas/newsjj/exceptions/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.joaolucas.newsjj.exceptions.handler;
 
 import com.joaolucas.newsjj.exceptions.ExceptionResponseBody;
+import com.joaolucas.newsjj.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,5 +24,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(errorCode).body(response);
     }
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionResponseBody> handleResourceNotFoundException(ResourceNotFoundException exception){
+        String error = HttpStatus.NOT_FOUND.name();
+        int errorCode = HttpStatus.NOT_FOUND.value();
+        String message = exception.getMessage();
+        LocalDateTime timestamp = LocalDateTime.now();
+
+        ExceptionResponseBody response = new ExceptionResponseBody(error, errorCode, message, timestamp);
+
+        return ResponseEntity.status(errorCode).body(response);
+    }
+
     
+
+
 }
