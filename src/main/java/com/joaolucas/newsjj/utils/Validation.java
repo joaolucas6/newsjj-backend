b171e5741afa3419f300139ebe37878db3772bac
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-09 20:04:54 -0300

    Creating isUserInfoValid

--- src/main/java/com/joaolucas/newsjj/utils/Validation.java
+++ src/main/java/com/joaolucas/newsjj/utils/Validation.java
@@ -1,5 +1,24 @@
 package com.joaolucas.newsjj.utils;
 
+import com.joaolucas.newsjj.model.dto.UserDTO;
+
+import java.time.LocalDate;
+
 public class Validation {
+    public boolean isUserInfoValid(UserDTO request){
+
+        if(request.getFirstName() != null && request.getFirstName().isBlank() || request.getFirstName() != null && request.getFirstName().length() > 25) return false;
+        if(request.getLastName() != null && request.getLastName().isBlank() || request.getLastName() != null && request.getLastName().length() > 25) return false;
+        if(request.getUsername() != null && request.getUsername().isBlank() || request.getUsername() != null && request.getUsername().length() > 15) return false;
+        if(request.getBio() != null && request.getBio().isBlank() || request.getBio() != null && request.getBio().length() > 160) return false;
+        if(request.getBirthDate() != null && request.getBirthDate().isAfter(LocalDate.now().minusYears(12))) return false;
+        if(request.getProfilePicUrl() != null && request.getProfilePicUrl().isBlank()) return false;
+
+        return true;
+    }
+
     
+
+
+
 }
