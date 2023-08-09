f5ca0ac75c0755b5e7acbd8dc5f849e0ce0d8ff6
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-09 20:21:39 -0300

    Creating isAllFieldsNull

--- src/main/java/com/joaolucas/newsjj/utils/Validation.java
+++ src/main/java/com/joaolucas/newsjj/utils/Validation.java
@@ -2,11 +2,12 @@ package com.joaolucas.newsjj.utils;
 
 import com.joaolucas.newsjj.model.dto.UserDTO;
 
+import java.lang.reflect.Field;
 import java.time.LocalDate;
 
 public class Validation {
-    public boolean isUserInfoValid(UserDTO request){
-
+    public boolean isUserInfoValid(UserDTO request) throws IllegalAccessException {
+        if(isAllFieldsNull(request)) return false;
         if(request.getFirstName() != null && request.getFirstName().isBlank() || request.getFirstName() != null && request.getFirstName().length() > 25) return false;
         if(request.getLastName() != null && request.getLastName().isBlank() || request.getLastName() != null && request.getLastName().length() > 25) return false;
         if(request.getUsername() != null && request.getUsername().isBlank() || request.getUsername() != null && request.getUsername().length() > 15) return false;
@@ -17,8 +18,19 @@ public class Validation {
         return true;
     }
 
-    
 
 
+    private boolean isAllFieldsNull(Object object) throws IllegalAccessException {
+
+        Field[] fields = object.getClass().getDeclaredFields();
+
+        for(Field field : fields){
+            field.setAccessible(true);
+
+            if(field.get(object) != null) return false;
+        }
+
+        return true;
+    }
 
 }
